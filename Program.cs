c49d834be11397d7e0db67d154057f24b6da50ec
Chris Smith <csmith@hashicorp.com>
2021-11-16 12:49:00 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vault_azure_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
